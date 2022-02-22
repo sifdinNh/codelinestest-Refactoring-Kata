ea8e8b53e0bf166a062644597cd4cd38796adb9f
sifdinNh <sifdinnh@gmail.com>
2022-02-23 00:42:33 +0100

    Refactor the B-DAWG Keychain statements

--- Python/gilded_rose.py
+++ Python/gilded_rose.py
@@ -7,11 +7,12 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name == "Good Wine" and item.name != "Backstage passes for Re:Factor" \
+            if item.name == "B-DAWG Keychain":
+                continue
+            if item.name != "Good Wine" and item.name != "Backstage passes for Re:Factor" \
                     and item.name != "Backstage passes for HAXX":
                 if item.quality > 0:
-                    if item.name != "B-DAWG Keychain":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -22,19 +23,17 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "B-DAWG Keychain":
-                item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Good Wine":
                     if item.name != "Backstage passes for Re:Factor" and item.name != "Backstage passes for HAXX":
                         if item.quality > 0:
-                            if item.name != "B-DAWG Keychain":
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+            item.sell_in = item.sell_in - 1
 
 
 class Item:
