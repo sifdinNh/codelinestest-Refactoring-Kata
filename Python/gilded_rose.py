c67f1d1d9398001aa26f7c1aedfabdded7d1170d
sifdinNh <sifdinnh@gmail.com>
2022-02-23 14:14:46 +0100

    refactoring passed sell_in statement

--- Python/gilded_rose.py
+++ Python/gilded_rose.py
@@ -9,7 +9,9 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "B-DAWG Keychain":
                 continue
-            elif item.quality < 50 :
+            item.sell_in = item.sell_in - 1  
+            
+            if item.quality < 50 :
                 if item.name == "Good Wine" :
                         item.quality = item.quality + 1
                 elif item.name in ["Backstage passes for Re:Factor" ,"Backstage passes for HAXX"]:
@@ -22,18 +24,17 @@ class GildedRose(object):
                                item.quality = item.quality + 1
                 elif item.quality > 0:
                     item.quality = item.quality - 1
-                    
+
             if item.sell_in < 0:
-                if item.name != "Good Wine":
-                    if item.name != "Backstage passes for Re:Factor" and item.name != "Backstage passes for HAXX":
-                        if item.quality > 0:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+                if item.name == "Good Wine":
                     if item.quality < 50:
                         item.quality = item.quality + 1
-            item.sell_in = item.sell_in - 1
+                elif item.name in ["Backstage passes for Re:Factor" ,"Backstage passes for HAXX"]:
+                        item.quality = item.quality - item.quality
+                else:
+                    if item.quality > 0:
+                                item.quality = item.quality - 1
+            
 
 
 class Item:
